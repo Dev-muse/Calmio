0ab87e0f557bbad3876171ee7ac2c537dc092ee8
A Muse <museabdirahman@gmail.com>
2021-08-09 03:33:15 +0100

    update ui

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,65 @@
-// autoplay sound chrome
-navigator.mediaDevices.getUserMedia({ audio: true }).then(function (stream) {
-  let bgSound = document.getElementById("myAudio");
-  bgSound.play();
+// selecting DOM elements
+const circleProgress = document.querySelector(".circle-progress");
+const numOfBreaths = document.querySelector(".breath-input");
+const start = document.querySelector(".start");
+const instructions = document.querySelector(".instructions");
+const breathsText = document.querySelector(".breaths-text");
 
-  // stop microphone stream acquired by getUserMedia
-  stream.getTracks().forEach(function (track) {
-    track.stop();
-  });
+// breaths remaining
+let breathsLeft = 3;
+
+// Watching for number of breaths selection change
+numOfBreaths.addEventListener("change", function (e) {
+  breathsLeft = numOfBreaths.value;
+  breathsText.innerText = breathsLeft;
+});
+
+// Grow/Shrink circle
+
+const growCircle = () => {
+  circleProgress.classList.add("circle-grow");
+  setTimeout(() => {
+    circleProgress.classList.remove("circle-grow");
+  }, 8000);
+};
+
+// Breathing Instructions
+
+const breatheTextUpdate = () => {
+  breathsLeft--;
+  breathsText.innerText = breathsLeft;
+  instructions.innerText = "Breathe In";
+  setTimeout(() => {
+    instructions.innerText = "Hold Your Breath";
+    setTimeout(() => {
+      instructions.innerText = "Exhale Slowly";
+    }, 4000);
+  }, 4000);
+};
+
+// Breathing App function
+const breathingApp = () => {
+  const breathingAnimation = setInterval(() => {
+    if (breathsLeft === 0) {
+      clearInterval(breathingAnimation);
+      return;
+    } else {
+      growCircle();
+      breatheTextUpdate();
+    }
+  }, 12000);
+};
+
+// start breathing
+start.addEventListener("click", () => {
+  start.classList.add("button-inactive");
+  instructions.innerText = "Get relaxed and ready to begin the breathing exercise";
+  setTimeout(() => {
+    instructions.innerText = "Ok exercise is starting...";
+    setTimeout(() => {
+      breathingApp();
+      growCircle();
+      breatheTextUpdate();
+    }, 2000);
+  }, 2000);
 });
